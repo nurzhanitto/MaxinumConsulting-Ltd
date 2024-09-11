202aac31400481e87bc4f804db8eab322f992c4c
Dexter <nurzanilasov@gmail.com>
2024-09-11 12:10:37 +0600

    Некоторые изменения

--- index.js
+++ index.js
@@ -1,38 +1,40 @@
 function saveExpense(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const expense = {
-        date: document.getElementById('date').value,
-        amount: document.getElementById('amount').value,
-        category: document.getElementById('category').value,
-        comment: document.getElementById('comment').value,
-    };
+  const expense = {
+    date: document.getElementById('date').value,
+    amount: document.getElementById('amount').value,
+    category: document.getElementById('category').value,
+    comment: document.getElementById('comment').value,
+  };
 
-    let expenses = JSON.parse(localStorage.getItem('expenses')) || [];
-    expenses.push(expense);
-    localStorage.setItem('expenses', JSON.stringify(expenses));
+  let expenses = JSON.parse(localStorage.getItem('expenses')) || [];
+  expenses.push(expense);
+  localStorage.setItem('expenses', JSON.stringify(expenses));
 
-    alert('Расход сохранен!');
-    document.getElementById('expenseForm').reset();
+  alert('Расход сохранен!');
+  document.getElementById('expenseForm').reset();
 
-    displayExpenses();
+  displayExpenses();
 }
 
 function displayExpenses() {
-    const expenses = JSON.parse(localStorage.getItem('expenses')) || [];
-    const expensesList = document.getElementById('expensesList');
+  const expenses = JSON.parse(localStorage.getItem('expenses')) || [];
+  const expensesList = document.getElementById('expensesList');
 
-    expensesList.innerHTML = expenses.map((expense, index) => `
+  expensesList.innerHTML = expenses.map((expense, index) => `
       <li class="list-group-item">
-        <strong>Дата:</strong> ${expense.date} | 
-        <strong>Сумма:</strong> ${expense.amount} | 
-        <strong>Категория:</strong> ${expense.category} | 
-        <strong>Комментарий:</strong> ${expense.comment}
+        <div class="d-flex justify-content-between">
+      <span><strong>Дата:</strong> ${expense.date}</span>
+      <span><strong>Сумма:</strong> ${expense.amount}</span>
+      <span><strong>Категория:</strong> ${expense.category}</span>
+      <span><strong>Комментарий:</strong> ${expense.comment}</span>
+    </div>
       </li>
     `).join('');
 }
 
 window.onload = function () {
-    document.getElementById('expenseForm').addEventListener('submit', saveExpense);
-    displayExpenses();
+  document.getElementById('expenseForm').addEventListener('submit', saveExpense);
+  displayExpenses();
 };
